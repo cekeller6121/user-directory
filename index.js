9620de7c6ca6e5916b7f2727fa0cdeb32f4ea695
chadwick <cekeller6121@yahoo.com>
2017-06-22 13:25:45 -0400

    add css styling

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ console.log("Hello, Clement.");
 
 const express = require('express');
 const app = express();
-const data = require('data');
+const users = require('./data.js');
+const mustacheExpress = require('mustache-express');
+app.use(express.static('./'));
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './')
+app.set('view engine', 'mustache')
 
 app.get('/', function(req, res) {
-  res.send("We're rollin...");
-  res.send(data);
+  res.render('index', users);
+});
+
+// app.get('/:username', function(req, res) {
+//   res.render('user', {users:/:username});
+// });
+
+app.get('/:username', function (req, res) {
+  const user = users.users.find(function (user) { return user.username == req.params.username});
+  res.render('user', {user: user});
 });
 
 app.listen(3000, function() {
